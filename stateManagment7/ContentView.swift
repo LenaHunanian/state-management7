adf4035fc08ad9c2d071d09d51693b917c0635a3
LenaHunanian <lenasss20053112@gmail.com>
2025-08-14 18:09:05 +0400

    Update ContentView.swift

--- stateManagment7/ContentView.swift
+++ stateManagment7/ContentView.swift
@@ -7,28 +7,30 @@
 
 import SwiftUI
 
+import SwiftUI
+
 struct ContentView: View {
     @State private var counterObject = CounterObject()
     
     var body: some View {
-        VStack {
-            Spacer()
+        VStack(spacing: 40){
             FirstChildView(counterObj: counterObject)
-            Spacer()
+            
             SecondChildView(counterObj: counterObject)
-            Spacer()
+            
             
             //just optional "reset" button from my side :)
             Button("Reset") {
                 counterObject.counter = 0
             }
+            .tint(.red)
         }
         .padding()
     }
 }
 
 struct FirstChildView: View {
-    var counterObj: CounterObject
+  @Bindable  var counterObj: CounterObject
     
     var body: some View {
         VStack(spacing: 20){
@@ -44,7 +46,7 @@ struct FirstChildView: View {
 }
 
 struct SecondChildView: View {
-    var counterObj: CounterObject
+  @Bindable  var counterObj: CounterObject
     var body: some View {
         VStack(spacing: 20){
             Text("Incremented count from second child view: \(counterObj.counter)")
@@ -55,6 +57,11 @@ struct SecondChildView: View {
         
     }
 }
+
+#Preview {
+    ContentView()
+}
+
 
 #Preview {
     ContentView()
